1f366cac06175081ed4e9db354a8f36e798da212
manansh11 <47335622+manansh11@users.noreply.github.com>
2020-08-16 14:03:16 -0600

    initial commit

---
+++ CryptoCurrency/blockchain.py
@@ -0,0 +1,97 @@
+from hashlib import sha256
+
+
+
+def updateHash(*args):
+    hashing_text = ""; h = sha256()
+    for arg in args:
+        hashing_text += str(arg)
+
+    h.update(hashing_text.encode())
+    return h.hexdigest()
+
+
+
+class Block():
+    
+
+
+    def __init__(self, number = 0, previous_hash = "0" * 64, data=None, nonce = 0):
+        self.data = data
+        self.number = number
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        
+    def hash(self):
+       return updateHash(
+
+                        self.previous_hash, 
+                        self.number, 
+                        self.data, 
+                        self.nonce
+                    )
+
+    def __str__(self):
+        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" 
+                    %(self.number, self.hash(), self.previous_hash, self.data, self.nonce))
+
+
+
+class Blockchain():
+    difficulty = 4
+
+
+    def __init__(self):
+        self.chain = []
+
+
+    # Add to blockchain
+    def add(self, block):
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
+
+    def mine(self, block):
+        try:
+            block.previous_hash = self.chain[-1].hash()
+        except IndexError:
+            pass
+
+        while True:
+            # Check if first 4 / OR DIFFICULTY LEVEL / characters of our block hash are 0's 
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
+                self.add(block); break
+            else:
+                block.nonce += 1
+
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+
+            if _previous != _current or _current[:self.difficulty] != "0" * self.difficulty:
+                return False
+        return True
+
+
+def main():
+    
+    blockchain = Blockchain()
+    database = ["Hello WOrld", "Gay", "fuck off", "six"]
+
+    num = 0
+    for data in database:
+        num += 1
+        blockchain.mine(Block(data, num))
+
+    for block in blockchain.chain:
+        print(block)
+
+
+    print(blockchain.isValid())
+    
+
+
+if __name__ == "__main__":
+    main()
