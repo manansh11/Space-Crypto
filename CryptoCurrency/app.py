fb616e6ad2cf3641c146b71ea46848193a75d15f
Manansh Shukla <47335622+manansh11@users.noreply.github.com>
2020-08-18 21:18:31 -0600

    Update app.py

--- CryptoCurrency/app.py
+++ CryptoCurrency/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Ronaldo1!'
+app.config['MYSQL_PASSWORD'] = '###'
 app.config['MYSQL_DB'] = 'crypto'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
